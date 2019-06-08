1109b42b4a0f79360b4bff9737fb33569be7e13e
guguco <m.sakaguchi.gugu@gmail.com>
2019-06-08 20:42:36 +0900

    fix some

--- src/router.js
+++ src/router.js
@@ -20,6 +20,11 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('./views/MyPage.vue')
+    },
+    {
+      path: '/projectlist',
+      name: 'projectlist',
+      component: () => import('./views/ProjectList.vue')
     }
   ]
 })
